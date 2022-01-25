795db3eb4590b832971dfb361349374453fae75f
Vladislav <vladlipchey05@gmail.com>
2022-01-25 22:45:34 +0200

    feat: add decode func

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var array=[];
+    var array2=[];
+    var str='';
+    for(let i=0;i<expr.length;i+=10){
+        var sli=expr.slice(i, i + 10);
+        array.push(sli);}
+    for(let i=0;i<array.length;i++){
+        for(let j=0;j<array[i].length;j++){
+            if(array[i][j]=="1" && array[i][j+1]=="0") {str+=".";
+        j++;}
+        if(array[i][j]=="1" && array[i][j+1]=="1") {str+="-";
+        j++;}
+            if (array[i][j]=='0' && array[i][j+ 1] == '0') {
+                j++;
+                continue;
+            }
+            if(array[i][j]=="*"){str+=' ';  break;}
+            
+        }
+        array2.push(str);
+    str='';
+    }
+    
+    for (let i = 0; i < array2.length; i++) {
+        if (array2[i] === ' ') {
+          str += ' ';
+        } else {
+          str += MORSE_TABLE[array2[i]];
+        }
+      }
+    
+      return str;
+    
 }
+
 
 module.exports = {
     decode
